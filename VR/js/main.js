a9b85eca81c5ab59d15f56de4cc755a76505745b
AlShiga <59224398+AlShiga@users.noreply.github.com>
2020-09-16 18:23:26 +0300

    16/09/20

--- VR/js/main.js
+++ VR/js/main.js
@@ -11,3 +11,57 @@ function removePreload() {
     }
 
 }
+
+function listScrollPage() {
+    var winScr = $(window).scrollTop() + $(window).height() * 0.7;
+    var currentNum = 0;
+    var maxScr = '';
+    $("[data-num]").each(function(index, element) {
+        var itemNum = $(element).attr("data-num");
+        var itemScr = $(element).offset().top - winScr;
+        if (maxScr == '' || (itemScr > maxScr && itemScr < 0)) {
+            maxScr = itemScr;
+            currentNum = itemNum;
+        }
+    });
+    return currentNum
+
+}
+
+
+
+function editScrollBar(num) {
+    $(".scrollBar__countСurrent  span").text(num);
+    $(".scrollBar__lineCurrent").css("transform", "translateY(" + (num - 1) * 100 + "%)")
+}
+
+$(window).scroll(function() {
+    var numPage = listScrollPage()
+    editScrollBar(numPage)
+});
+
+function listScrollPage() {
+    var winScr = $(window).scrollTop() + $(window).height() * 0.7;
+    var currentNum = 0;
+    var maxScr = '';
+    $("[data-num]").each(function(index, element) {
+        var itemNum = $(element).attr("data-num");
+        var itemScr = $(element).offset().top - winScr;
+        if (maxScr == '' || (itemScr > maxScr && itemScr < 0)) {
+            maxScr = itemScr;
+            currentNum = itemNum;
+        }
+    });
+    return currentNum
+}
+
+function editScrollBar(num) {
+    if (num < 10) {
+        num = "0" + num
+    }
+    $(".scrollBar__countСurrent  span").text(num);
+    $(".scrollBar__lineCurrent").css("transform", "translateY(" + (num - 1) * 100 + "%)")
+}
+$('.burgerOpen').click(function() {
+    $('body').toggleClass("openMenu");
+});
