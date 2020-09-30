57bec1173a337753c67857201495feffb07505e0
AlShiga <59224398+AlShiga@users.noreply.github.com>
2020-09-30 16:21:21 +0300

    30.09.20/3

--- GT/js/main.js
+++ GT/js/main.js
@@ -178,6 +178,71 @@ btnNext.onclick = function() {
 
 
 
+
+
+function addOnWheel(elem, handler) {
+    if (elem.addEventListener) {
+        if ('onwheel' in document) {
+            // IE9+, FF17+
+            elem.addEventListener("wheel", handler);
+        } else if ('onmousewheel' in document) {
+            // устаревший вариант события
+            elem.addEventListener("mousewheel", handler);
+        } else {
+            // 3.5 <= Firefox < 17, более старое событие DOMMouseScroll пропустим
+            elem.addEventListener("MozMousePixelScroll", handler);
+        }
+    } else { // IE8-
+        text.attachEvent("onmousewheel", handler);
+    }
+}
+
+
+let firstEl = document.querySelector(".firstSection");
+
+addOnWheel(firstEl, function(e) {
+    var elm = document.querySelector(".sectionVideo");
+    console.log("Круть")
+        // отменим прокрутку
+    e.preventDefault();
+    elm.scrollIntoView({ block: "start", behavior: "smooth" });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ! function() {
     "use strict";
 
