a3f0a98494581a2e22e8d4968e535c423e7ea5d9
AlShiga <59224398+AlShiga@users.noreply.github.com>
2021-05-26 16:26:08 +0300

    add rrt

---
+++ RRT/js/main.js
@@ -0,0 +1,49 @@
+
+// const accordeon = {
+//   init: function(){
+//     if(document.querySelector('.accordeon')){
+//       accordeon.heads = document.querySelectorAll('.accordeon__head');
+//       accordeon.heads.forEach((el)=>{
+//         el.addEventListener('click', accordeon.showAcc);
+//       })      
+//     }
+//   },
+//   showAcc: function(evt){
+//       accordeon.heads.forEach((el)=>{
+//         el.classList.remove('open');
+//       })
+//       evt.currentTarget.classList.add('open');
+//   }
+// }
+// document.addEventListener('DOMContentLoaded',()=>{
+//   accordeon.init();
+//   moveBtn.init();
+//   invertEl.initInv();
+// });
+
+
+const accordeon = {
+  init: function(){
+    this.wrap = document.querySelectorAll('.accordeon');
+    if(this.wrap.length){
+      this.wrap.forEach((wrapI)=>{
+        wrapI.addEventListener('click', (e)=>{
+            if(e.target.classList.contains("accordeon__head")|| e.target.closest('.accordeon__head')){
+              
+            }
+        });
+      })
+    }
+  },
+  showAcc: function(evt){
+    accWr.querySelectorAll('.accordeon__head').forEach((el)=>{
+      el.classList.remove('open');
+    })
+    console.log(evt)
+    console.dir(evt)
+    evt.currentTarget.classList.add('open');
+  }
+}
+document.addEventListener('DOMContentLoaded',()=>{
+  accordeon.init();
+});
