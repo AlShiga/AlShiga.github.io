7d8cc553923d9caf00f1f6e7c9b643e2fc288940
AlShiga <59224398+AlShiga@users.noreply.github.com>
2021-06-30 08:14:07 +0300

    30/06

--- RRT/js/main.js
+++ RRT/js/main.js
@@ -1,24 +1,84 @@
-const scroll = new LocomotiveScroll({
+gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   repeat: true,
   smooth: true,
-  lerp: 0.1,
+  smoothMobile: true,
+  lerp: .07, 
+  // lerp: 0.1,
+  reloadOnContextChange: true
 });
-new Accordion('.accordion-container',{
-  duration: 400,
-  showMultiple: true,
-  onOpen: function() {
-    scroll.update();
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
-  onClose: function() {
-    scroll.update();
-  }
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
 });
-scroll.update();
+
+document.querySelectorAll(".imgParalax").forEach(el=>{
+
+  gsap.from(el.querySelector('img'), {
+    scrollTrigger: {
+      trigger: el,
+      scroller: "[data-scroll-container]",
+      scrub: true,
+      start: "top bottom",
+      end: "top top",
+      // onUpdate: self => console.log(self.direction)
+    },
+    translateY: "-50%",
+    transformOrigin: "left center", 
+    ease: "none"
+  });
+
+})
+
+gsap.to(".aGal__wrapInner", {
+  scrollTrigger: {
+    trigger: ".aGal",
+    scroller: "[data-scroll-container]",
+    scrub: true,
+    start: "top top",
+    end: "bottom bottom",
+    // onUpdate: self => console.log(self.direction)
+  },
+  translateX: "-50%",
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+
+
+
+
+
+let accord = document.querySelectorAll(".accordion-container")
+function initAccordeon(){
+  if(!accord.length)return;
+  new Accordion('.accordion-container',{
+    duration: 400,
+    showMultiple: true,
+    onOpen: function() {
+      scroll.update();
+    },
+    onClose: function() {
+      scroll.update();
+    }
+  });
+  scroll.update();
+}
+
 let head = document.querySelector('header')
 function hideMenu(head){
   if(!head)return;
-  scroll.on("scroll", function(t) {
+  locoScroll.on("scroll", function(t) {
   // console.log(t.delta.y)
   t.delta.y<200? 
     !head.classList.contains('header_full')?
@@ -114,3 +174,5 @@ const animBtn={
   }
 }
 animBtn.init();
+
+
